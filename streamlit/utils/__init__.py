db590ad84a6502c0d03f1a3ab6d4dd42c9bc463b
Guillaume Lostis <guillaume.lostis@preligens.com>
2023-11-15 19:01:30 +0100

    Update Streamlit data exploration

--- streamlit/utils/__init__.py
+++ streamlit/utils/__init__.py
@@ -6,3 +6,18 @@ TABLE = "flights"
 DB_PATH = Path("db.db")
 LOG_PATH = Path("log.log")
 TZ = pytz.timezone("Europe/Paris")
+
+AIRBORNE_WHERE = "(on_ground = 0) and (ground_speed >= 20)"
+
+
+def db_query(db_conn, query, where=None, groupby=None, limit=None, ttl=3600):
+    sql = query
+    sql += f" from {TABLE}"
+    if where:
+        sql += f" where {where}"
+    if groupby:
+        sql += f" group by {groupby}"
+    if limit:
+        sql += f" limit {limit}"
+    sql += ";"
+    return db_conn.query(sql, ttl=ttl)
